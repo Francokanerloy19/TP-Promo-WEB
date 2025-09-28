cc1887d7786a983bddda8fc733480ba6ef4c659f
Franco Kaner Loy <franco@gmail.com>
2025-09-28 18:05:38 -0300

    Se agregaron los campos DNI, Email, Nombre y Apellido

--- TP Promo WEB/Formulario.aspx
+++ TP Promo WEB/Formulario.aspx
@@ -1,3 +1,35 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Formulario.aspx.cs" Inherits="TP_Promo_WEB.Formulario" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="row">
+       
+        <div class="col mb-3">
+            <label for="txtId" class="form-label">DNI</label>
+            <asp:TextBox runat="server" ID="txtId" CssClass="form-control" placeholder="99888777" />
+        </div>
+    </div>
+
+    <div class="row">
+        
+        <div class="col mb-0">
+            <label for="TextBox1" class="form-label">Nombre</label>
+            <asp:TextBox runat="server" ID="TextBox1" CssClass="form-control" placeholder="Nombre" />
+        </div>
+        
+        <div class="col mb-0">
+            <label for="TextBox2" class="form-label">Apellido</label>
+            <asp:TextBox runat="server" ID="TextBox2" CssClass="form-control" placeholder="Apellido" />
+        </div>
+        
+        <div class="col mb-0">
+            <label for="txtEmail" class="form-label">Email</label>
+            <div class="input-group">
+                <span class="input-group-text" id="txtEmails">@</span>
+                <asp:TextBox runat="server" ID="txtEmail" CssClass="form-control" placeholder="email@email.com" />
+            </div>
+        </div>
+    </div>
+
+
 </asp:Content>
--- TP Promo WEB/Formulario.aspx.designer.cs
+++ TP Promo WEB/Formulario.aspx.designer.cs
@@ -13,5 +13,41 @@ namespace TP_Promo_WEB
 
     public partial class Formulario
     {
+
+        /// <summary>
+        /// Control txtId.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtId;
+
+        /// <summary>
+        /// Control TextBox1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+
+        /// <summary>
+        /// Control TextBox2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+
+        /// <summary>
+        /// Control txtEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmail;
     }
 }
