78f4df2aba7da910391fd0122361d8b0df51d18f
Emanuel Infante <pei2688@gmail.com>
2025-10-03 00:17:52 -0300

    cambio propiedad Descripcion a DescripcionMarca en Marca y ajusto método Listar

--- Negocio/ArticuloNegocio.cs
+++ Negocio/ArticuloNegocio.cs
@@ -30,11 +30,11 @@ namespace Negocio
 
                     aux.Marca = new Marca();
                     aux.Marca.IdMarca = (int)accesoDatos.Lector["IdMarca"];
-                    aux.Marca.Descripcion = (string)accesoDatos.Lector["DescripcionMarca"];
+                    aux.Marca.DescripcionMarca = (string)accesoDatos.Lector["DescripcionMarca"];
 
                     aux.Categoria = new Categoria();
                     aux.Categoria.IdCategoria = (int)accesoDatos.Lector["IdCategoria"];
-                    aux.Categoria.Descripcion = (string)accesoDatos.Lector["DescripcionCategoria"];
+                    aux.Categoria.DescripcionCategoria = (string)accesoDatos.Lector["DescripcionCategoria"];
 
                     ImagenNegocio imagen = new ImagenNegocio();
                     aux.Imagenes = imagen.ObtenerImagenesPorArticulo(aux.IdArticulo);
@@ -51,6 +51,59 @@ namespace Negocio
                 accesoDatos.cerrarConexion();
             }
             return Lista;   
+        }
+   
+        public Articulo BuscarArticuloPorId( int id)
+        {
+            Articulo articulo = null;
+            AccesoDatos accesoDatos = new AccesoDatos();
+
+            try
+            {
+                accesoDatos.setearConsulta("SELECT a.Id AS IdArticulo, a.Codigo AS CodigoArticulo, a.Nombre AS NombreArticulo, a.Descripcion AS DescripcionArticulo, a.IdMarca, m.Descripcion AS DescripcionMarca, a.IdCategoria, c.Descripcion AS DescripcionCategoria, a.Precio AS PrecioArticulo FROM ARTICULOS a INNER JOIN MARCAS m ON a.IdMarca = m.Id INNER JOIN CATEGORIAS c ON a.IdCategoria = c.Id WHERE a.Id = @id");
+               
+                accesoDatos.setearParametros("@Id", id);
+                accesoDatos.ejecutarConsulta();
+
+                if (accesoDatos.Lector.Read())
+                {
+                    articulo = new Articulo
+                    {
+                        IdArticulo = (int)accesoDatos.Lector["IdArticulo"],
+                        CodigoArticulo = (string)accesoDatos.Lector["CodigoArticulo"],
+                        NombreArticulo = (string)accesoDatos.Lector["NombreArticulo"],
+                        DescripcionArticulo = (string)accesoDatos.Lector["DescripcionArticulo"],
+                        PrecioArticulo = (decimal)accesoDatos.Lector["PrecioArticulo"]
+                    };
+
+                    articulo.Marca = new Marca
+                    {
+                        IdMarca = (int)accesoDatos.Lector["IdMarca"],
+                        DescripcionMarca = (string)accesoDatos.Lector["DescripcionMarca"]
+                    };
+
+                    articulo.Categoria = new Categoria
+                    {
+                        IdCategoria = (int)accesoDatos.Lector["IdCategoria"],
+                        DescripcionCategoria = (string)accesoDatos.Lector["DescripcionCategoria"]
+                    };
+
+                    ImagenNegocio imagen = new ImagenNegocio();
+                    articulo.Imagenes = imagen.ObtenerImagenesPorArticulo(articulo.IdArticulo);
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                accesoDatos.cerrarConexion();
+            }
+
+            return articulo;
         }
     }
 }
