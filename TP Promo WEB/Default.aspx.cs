ed3b76052ba13f192735a0c568a11d4a5cfe7812
Franco Kaner Loy <franco@gmail.com>
2025-10-03 22:12:35 -0300

    Se continuo con la validacion del voucher

--- TP Promo WEB/Default.aspx.cs
+++ TP Promo WEB/Default.aspx.cs
@@ -11,20 +11,35 @@ namespace TP_Promo_WEB
 {
     public partial class _Default : Page
     {
+
         protected void btnSiguiente_Click(object sender, EventArgs e)
         {
+            // Limpiar mensaje previo
+            lblMensajeError.Text = "";
+            lblMensajeError.CssClass = "";
+
             CuponNegocio negocio = new CuponNegocio();
             Cupon cupon = negocio.BuscarCupon(TextCodigo.Text);
 
-            if (cupon.idArticulo != null && cupon.fechaCanje != null && cupon.idClinte != null)
+            if (cupon == null)
             {
                 lblMensajeError.Text = "❌ El voucher no existe o es inválido.";
                 lblMensajeError.CssClass = "text-danger"; // rojo
+                return;
             }
             else
             {
-                lblMensajeError.Text = "✅ Voucher válido, ¡podés continuar!";
-                lblMensajeError.CssClass = "text-success"; // verde
+                if (cupon.idArticulo != null && cupon.fechaCanje != null && cupon.idClinte != null)
+                {
+                    lblMensajeError.Text = "❌ El voucher ya fue utilizado.";
+                    lblMensajeError.CssClass = "text-danger"; // rojo
+                }
+                else
+                {
+
+                    Response.Redirect("ListaDeArticulos.aspx", false);
+
+                }
             }
 
         }
